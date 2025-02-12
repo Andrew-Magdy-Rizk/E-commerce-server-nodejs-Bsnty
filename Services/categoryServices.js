30c399129cd8dae3186b615aa792eafddfd6ba83
Andrew Magdy <89481184+Andrew-Magdy-Rizk@users.noreply.github.com>
2025-02-12 12:25:37 +0200

    return bsnty

--- Services/categoryServices.js
+++ Services/categoryServices.js
@@ -13,46 +13,46 @@ import { uploadSingleImage } from "../Middlewares/uploadImageMiddleware.js";
 import cloudinary from "../Config/cloudinary.js";
 
 // Upload single image
-// export const uploadCategoryImage = uploadSingleImage("image");
+export const uploadCategoryImage = uploadSingleImage("image");
 
 // Image processing
-// export const resizeImage = asyncHandler(async (req, res, next) => {
-//   // const filename = `category-${uuidv4()}-${Date.now()}.jpeg`;
+export const resizeImage = asyncHandler(async (req, res, next) => {
+  // const filename = `category-${uuidv4()}-${Date.now()}.jpeg`;
 
-//   // if (req.file) {
-//   //   await sharp(req.file.buffer)
-//   //     .resize(600, 600)
-//   //     .toFormat("jpeg")
-//   //     .jpeg({ quality: 95 })
-//   //     .toFile(`uploads/categories/${filename}`);
+  // if (req.file) {
+  //   await sharp(req.file.buffer)
+  //     .resize(600, 600)
+  //     .toFormat("jpeg")
+  //     .jpeg({ quality: 95 })
+  //     .toFile(`uploads/categories/${filename}`);
 
-//   //   // Save image into our db
-//   // req.body.image = filename;
-//   // }
+  //   // Save image into our db
+  // req.body.image = filename;
+  // }
 
-//   if (req.file) {
-//     const imagesBuffer = await sharp(req.file.buffer)
-//       .resize(500)
-//       .toFormat("jpg")
-//       .jpeg({ quality: 95 })
-//       .toBuffer();
-//     console.log(imagesBuffer);
+  if (req.file) {
+    const imagesBuffer = await sharp(req.file.buffer)
+      .resize(500)
+      .toFormat("jpg")
+      .jpeg({ quality: 95 })
+      .toBuffer();
+    console.log(imagesBuffer);
 
-//     cloudinary.uploader
-//       .upload_stream({ folder: "categories" }, (error, result) => {
-//         if (error) {
-//           console.log(error);
-//           return next(error);
-//         } else {
-//           req.body.image = result.url;
-//           next();
-//         }
-//       })
-//       .end(imagesBuffer);
-//   } else {
-//     next();
-//   }
-// });
+    cloudinary.uploader
+      .upload_stream({ folder: "categories" }, (error, result) => {
+        if (error) {
+          console.log(error);
+          return next(error);
+        } else {
+          req.body.image = result.url;
+          next();
+        }
+      })
+      .end(imagesBuffer);
+  } else {
+    next();
+  }
+});
 
 export const getCategories = getAll(categorySchema);
 
